33aaa5a382a0a990980e7af8d5c3ca11a7ac608d
nancy sin <nsin@sbcglobal.net>
2018-01-17 20:50:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 function getFirstSelector(selector){
   return document.querySelector(selector)
 }
+
+function nestedTarget(){
+  return document.getElementById('nested').querySelector('div')
+}
+
+function increaseRankBy(){
+  var parsed = parseInt(n)
+const list = document.getElementById('app').querySelectorAll('ul.ranked-list')
+for (var = 0; i < list.length; i ++)(
+  list[i].innerHTML = (i + parsed);
+  }
+
+
+function deepestChild(){
+  return document.getElementById('grand-node').querySelector('div div div div div');
+}
